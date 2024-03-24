8dd05f48526ebec2dbb7bc913853b7da73949e78
bprs077 <163451315+bprs077@users.noreply.github.com>
2024-03-23 23:56:23 -0400

    Create index_t.tsx

---
+++ .templates/index_t.tsx
@@ -0,0 +1,15 @@
+// Copy, paste, change name, and use as the template for any new page (e.g., About, Contact, etc.)
+
+import Head from "next/head";
+import Image from "next/image";
+import Container from '../components/Container'
+
+export default function Index() {
+  return (
+    <>
+      <Container>
+        Index
+      </Container>
+    </>
+  );
+}
